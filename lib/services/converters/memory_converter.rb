44eb67dc3edf04284e5630993285cc8767acb78e
a04b04 <arun04b@gmail.com>
2026-06-10 14:35:12 +0100

    now allows for decimal GB

--- lib/services/converters/memory_converter.rb
+++ lib/services/converters/memory_converter.rb
@@ -1,20 +1,20 @@
 module MemoryConverter
 
     def self.to_mb(memory)
-        match = memory.strip.upcase.match(/\A(\d+)\s*(K|KB|M|MB|G|GB|T|TB)\z/)
+        match = memory.strip.upcase.match(/\A(\d+(?:\.\d+)?)\s*(M|MB|G|GB|T|TB)\z/)
 
         return nil unless match 
 
-        amount = match[1].to_i
+        amount = match[1].to_f
         unit = match[2]
 
         case unit 
         when"M", "MB"
-            amount
+            amount.ceil
         when "G", "GB"
-            amount * 1024 
+            amount.ceil * 1024 
         when "T", "TB"
-            amount * 1024 * 1024
+            amount.ceil * 1024 * 1024
         end
     end
 end
