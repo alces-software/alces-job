8fb350dbbd9e1fb903b2a339650ed586b537c581
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2026-06-11 15:20:14 +0100

    Update remove.rb

--- lib/cli/commands/config/remove.rb
+++ lib/cli/commands/config/remove.rb
@@ -13,8 +13,8 @@ module AlcesJob
         AlcesJob::CLI.register 'config remove', self
         desc 'Used to remove items from the the admin config file e.g. partitions, nodes'
 
-        option :nodes, type: :boolean, default: false, desc: 'Targets nodes'
-        option :partitions,  type: :boolean, default: false, desc: 'Targets partitions'
+        options :nodes, type: :string, default: '', desc: 'The nodes you want to remove from the system config'
+        options :partitions, type: :string, default: '', desc: 'The partitions you want to remove from the system config'
 
         def initialize
           config = YAML.load_file(File.expand_path('../../../../config.yaml', __dir__))
@@ -22,15 +22,56 @@ module AlcesJob
           @system_data = nil
         end
 
-        def call(**options)
-          selected = options.select { |_, v| v }.keys
+        def call(**_options)
+          pastel = Pastel.new
 
-          if selected.size != 1
-            puts 'You can only specify one target'
+          if Process.uid != 0
+            puts pastel.red("\nThis command must be ran with elevated privileges\n")
             exit(1)
           end
 
-          puts 'hello'
+          unless File.exist?(@config_path)
+            puts pastel.red("\nThere is no config to edit generate a config with config init\n")
+            exit(1)
+          end
+
+          spinner = TTY::Spinner.new(
+            '[:spinner] :title ...',
+            success_mark: pastel.green('✔'),
+            error_mark: pastel.red('✖')
+          )
+
+          spinner.update(title: 'removing from config')
+          spinner.auto_spin
+
+          @system_data = YAML.load_file(@config_path)
+
+          if options[:nodes]
+            nodes_to_delete = options[:nodes].split(',').map(&:strip)
+
+            @system_data['nodes'] = @system_data['nodes'].reject do |node|
+              nodes_to_delete.include?(node['node'])
+            end
+          end
+
+          if options[:partitions]
+            partitions_to_delete = options[:partitions].split(',').map(&:strip)
+
+            @system_data['partitions'] = @system_data['partitions'].reject do |partition|
+              partitions_to_delete.include?(partition['partition'])
+            end
+          end
+
+          spinner.success('(successful)')
+          spinner.update(title: 'updating config file')
+          spinner.auto_spin
+
+          File.write(@config_path, @system_data)
+
+          spinner.success('(successful)')
+
+          puts pastel.green("\nSuccessfully remove the items from the config\n")
+          exit(0)
         end
       end
     end
