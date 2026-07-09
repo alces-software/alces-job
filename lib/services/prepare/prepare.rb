d5d37cbc63424433e425e7c7bb08b0862e2f8e61
a04b04 <arun04b@gmail.com>
2026-07-09 15:57:15 +0100

    prep should probalby work

--- lib/services/prepare/prepare.rb
+++ lib/services/prepare/prepare.rb
@@ -4,22 +4,27 @@ module AlcesJob
   module Services
     class Prepare
       def self.directives
-        "#SBATCH --output=%x-%j.out\n#SBATCH --error=%x-%j.err\n"
+        ''
       end
 
       def self.helper
-        "alces_prepare_job() {\n  " \
-          "job_dir=\"$HOME/${SLURM_JOB_NAME}-${SLURM_JOB_ID}\"\n" \
-          "\n  " \
-          "if [ -n \"$SLURM_ARRAY_TASK_ID\" ]; then\n    " \
-          "job_dir=\"${job_dir}-${SLURM_ARRAY_TASK_ID}\"\n  " \
-          "fi\n" \
-          "\n  " \
-          "mkdir -p \"$job_dir\"\n  " \
-          "cd \"$job_dir\" || exit 1\n" \
-          "}\n" \
-          "\n" \
-          "alces_prepare_job\n"
+        <<~BASH
+          alces_prepare_job() {
+            job_dir="$HOME/${SLURM_JOB_NAME}-${SLURM_JOB_ID}"
+
+            if [ -n "$SLURM_ARRAY_TASK_ID" ]; then
+              job_dir="${job_dir}-${SLURM_ARRAY_TASK_ID}"
+            fi
+
+            mkdir -p "$job_dir"
+            cd "$job_dir" || exit 1
+
+            exec > "${job_dir}/${SLURM_JOB_NAME}-${SLURM_JOB_ID}.out"
+            exec 2> "${job_dir}/${SLURM_JOB_NAME}-${SLURM_JOB_ID}.err"
+          }
+
+          alces_prepare_job
+        BASH
       end
     end
   end
